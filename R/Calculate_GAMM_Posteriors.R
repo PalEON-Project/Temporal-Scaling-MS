f09be74db2c855d832d9f8080a1438c83b1fffee
crollinson <crolli@comm675-0401-dhcp161.bu.edu>
2015-06-02 17:48:17 -0400

    New Functions
    Created new generalized functions to facilitate calculating effect
    weights through time & generating CIs around gamm smoothers

---
+++ R/Calculate_GAMM_Posteriors.R
@@ -0,0 +1,48 @@
+post.distns <- function(model, newdata, n=1000, n2=25, terms=T, lwr=0.025, upr=0.975){
+	# Note: this function can be used to generate a 95% CI on the full model OR terms
+
+	# -----------
+	# Simulating a posterior distribution of Betas to get variance on non-linear functions
+	# This is following Gavin Simpson's post here: http://www.fromthebottomoftheheap.net/2011/06/12/additive-modelling-and-the-hadcrut3v-global-mean-temperature-series/
+	# -----------
+	library(MASS)
+	set.seed(321)
+
+	# If the model is a mixed model (gamm) rather than a normal gam, extract just the gam portion
+	if(class(model)[[1]]=="gamm") model <- model$gam
+
+
+
+	# Generate a random distribution of betas
+	Rbeta <- mvrnorm(n=n, coef(model), vcov(model))
+
+	# Create the prediction matrix
+	Xp <- predict(model, newdata=newdata, type="lpmatrix")
+
+	# Some handy column indices
+	# cols.site <- which(substr(names(coef.gam),1,4)=="Site")
+	cols.site   <- 1 # Running on 1 site so the "site" intercept is always 1
+	cols.temp   <- which(substr(names(coef.gam),1,7)=="s(Temp)")
+	cols.precip <- which(substr(names(coef.gam),1,9)=="s(Precip)")
+	cols.co2    <- which(substr(names(coef.gam),1,6)=="s(CO2)")
+
+	want <- sample(n, n2) # sample n2 number of ranges from sim1
+	
+	if(terms==T){
+		sim.temp   <- Xp[,cols.temp]   %*% t(Rbeta[,cols.temp]) 
+		sim.precip <- Xp[,cols.precip] %*% t(Rbeta[,cols.precip]) 
+		sim.co2    <- Xp[,cols.co2]    %*% t(Rbeta[,cols.co2]) 
+		
+		df.out <- list()
+		df.out[["Temp"]] <- data.frame(mean=apply(sim.temp, 1, mean), lwr=apply(sim.temp, 1, quantile, lwr), upr=apply(sim.temp, 1, quantile, upr))
+		df.out[["Precip"]] <- data.frame(mean=apply(sim.precip, 1, mean), lwr=apply(sim.precip, 1, quantile, lwr), upr=apply(sim.precip, 1, quantile, upr))
+		df.out[["CO2"]] <- data.frame(mean=apply(sim.co2, 1, mean), lwr=apply(sim.co2, 1, quantile, lwr), upr=apply(sim.co2, 1, quantile, upr))
+
+	} else {
+		sim1 <- Xp %*% t(Rbeta) # simulates n predictions of the response variable in the model
+		
+		df.out <- data.frame(mean=apply(sim1, 1, mean), lwr=apply(sim1, 1, quantile, lwr), upr=apply(sim1, 1, quantile, upr))
+	}
+
+	return(df.out)
+}
---
+++ R/Calculate_GAMM_Weights.R
@@ -0,0 +1,56 @@
+factor.weights <- function(model, newdata){
+	# If the model is a mixed model (gamm) rather than a normal gam, extract just the gam portion
+	if(class(model)[[1]]=="gamm") model <- model$gam
+	# -----------
+	# calculating the weights for each of the factors
+	# -----------
+	# Create the prediction matrix
+	Xp <- predict(model, newdata=newdata, type="lpmatrix")
+
+	fit <- Xp %*% coef(model) # The full predicted values; used for model QA/QC
+	coef.gam <- coef(model) # the gam coefficients
+	
+	# Some handy column indices
+	# cols.site <- which(substr(names(coef.gam),1,4)=="Site")
+	cols.site   <- 1 # Running on 1 site so the "site" intercept is always 1
+	cols.temp   <- which(substr(names(coef.gam),1,7)=="s(Temp)")
+	cols.precip <- which(substr(names(coef.gam),1,9)=="s(Precip)")
+	cols.co2    <- which(substr(names(coef.gam),1,6)=="s(CO2)")
+
+	# calculating the smoother for each effect
+	fit.int    <- Xp[,cols.site]    *  coef.gam[cols.site] # Note: no matrix multiplication because it's 1 x 1
+	fit.temp   <- Xp[,cols.temp]   %*% coef.gam[cols.temp]
+	fit.precip <- Xp[,cols.precip] %*% coef.gam[cols.precip]
+	fit.co2    <- Xp[,cols.co2]    %*% coef.gam[cols.co2]
+
+	# Calculated the SD around each smoother
+	sd.int    <-    sum (Xp[,cols.site]    *  model$Vp[cols.site, cols.site]    *Xp[,cols.site]    )^0.5
+	sd.temp   <- rowSums(Xp[,cols.temp]   %*% model$Vp[cols.temp, cols.temp]    *Xp[,cols.temp]    )^0.5
+	sd.precip <- rowSums(Xp[,cols.precip] %*% model$Vp[cols.precip, cols.precip]*Xp[, cols.precip] )^0.5
+	sd.co2    <- rowSums(Xp[,cols.co2]    %*% model$Vp[cols.co2, cols.co2]      *Xp[, cols.co2]    )^0.5
+
+	# Summing the fixed effects to do QA/QC and throwing a warning if it's not very close to the predicted values
+	fit.sum <- fit.int + fit.co2 + fit.temp + fit.precip
+	fit.spline <- fit.co2 + fit.temp + fit.precip
+	if(max(abs(fit - fit.sum))>1e-4) print("***** WARNING: sum of fixed effects not equal to predicted value *****")
+
+	# summing the absolute values to get the weights for each fixed effect
+	fit.sum2 <- abs(fit.int) + abs(fit.co2) + abs(fit.temp) + abs(fit.precip)
+	fit.spline2 <- abs(fit.co2) + abs(fit.temp) + abs(fit.precip)
+
+	# Factor weights are determined by the relative strength of Temp, Precip, & CO2
+	df.weights <- data.frame(Temp=newdata$Temp, Precip=newdata$Precip, CO2=newdata$CO2, fit=fit, fit.intercept=fit.int, fit.temp=fit.temp, fit.precip=fit.precip, fit.co2=fit.co2, sd.temp=sd.temp, sd.precip=sd.precip, sd.co2=sd.co2,  fit.spline=fit.spline, weight.co2=fit.co2/fit.spline2, weight.temp=fit.temp/fit.spline2, weight.precip=fit.precip/fit.spline2)
+	# Add in a couple factors that are useful if not predicting on the old data
+	if(!is.null(newdata$Year)) df.weights$Year=newdata$Year 
+	if(!is.null(newdata$Site)) df.weights$Year=newdata$Site 
+
+	# doing a little bit of handy-dandy calculation to give a flag as to which factor is given the greatest weight in a given year
+	for(i in 1:nrow(df.weights)){
+		fweight <- abs(df.weights[i,c("weight.co2", "weight.temp", "weight.precip")])
+		df.weights[i,"max"] <- max(fweight)
+		df.weights[i,"factor.max"] <- c("co2", "temp", "precip")[which(fweight==max(fweight))]
+	}
+	df.weights$factor.max <- as.factor(df.weights$factor.max)
+	# summary(df.weights)
+	return(df.weights)
+}
