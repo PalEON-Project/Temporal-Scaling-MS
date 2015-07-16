a6242f78ea0c6c6ba1dc98441f35a20a5be75b3b
crollinson <crollinson@gmail.com>
2015-07-16 10:22:04 -0400

    completed data table/figure for correlation across scales

--- R/4_analyze_drivers.R
+++ R/4_analyze_drivers.R
@@ -44,6 +44,7 @@ library(zoo)
 # ----------------------------------------
 # setwd("~/Desktop/Research/PalEON CR/PalEON_MIP_Site/Analyses/Temporal-Scaling")
 setwd("..")
+path.data <- "Data"
 out.dir <- "Data/analysis_drivers"
 fig.dir <- "Figures/analysis_drivers"
 
@@ -90,14 +91,7 @@ dev.off()
 # Look at driver correlations
 # ----------------------------------------
 # Make some simple plots of correlation
-# pdf(file.path(fig.dir, "Driver_Correlations_by_Scale.pdf"))
-# for(s in unique(drivers$Scale)){
-	# print(
-	# plot(drivers[drivers$Scale==s, vars.met], main=paste0("Met Driver Correlations, ", s))
-	# )
-# }
-# dev.off()
-
+# Note: because of the number of data points, these need to be saved in a "flattened" format
 for(s in unique(drivers$Scale)){
 	png(file.path(fig.dir, paste0("Driver_Correlations_",s,".png")))
 	plot(drivers[drivers$Scale==s, vars.met], main=paste0("Met Driver Correlations, ", s))
@@ -108,57 +102,46 @@ for(s in unique(drivers$Scale)){
 scales <- unique(drivers$Scale)
 vars.met
 
-test <- merge(vars.met, vars.met)
-data.temp <- drivers[driver$scale==scales[s],]
-cor.scales <- data.frame(pair   = paste(vars.met[1], vars.met[2], sep=":"), 
-						 scale  = scales[s],
-						 cor.est= cor(data.temp[,vars.met[1:2]], use="pairwise.complete.obs")[2,2],
-						 cor.lo = cor.test(data.temp[,vars.met[1]], data.temp[,vars.met[2]], use="pairwise.complete.obs")$conf.int[1],
-						 cor.hi = cor.test(data.temp[,vars.met[1]], data.temp[,vars.met[2]], use="pairwise.complete.obs")$conf.int[2],
-						 )
+# Need to replae vars.met with something that lists the relevant pairs without doing the redundnat pairs
 for(s in 1:length(scales)){
-  data.temp <- drivers[drivers$Scale==scales[s],]
-  cor.temp <- data.frame(pair   = paste(vars.met[1], vars.met[2], sep=":"), 
+	data.temp <- drivers[drivers$Scale==scales[s],]
+
+	# to get proper pairing we need each variable matched with the ones we haven't done yet so
+	for(i in 1:(length(vars.met)-1)){
+		for(j in (i+1):length(vars.met)){
+			# putting the correlation info into a data frame
+			cor.temp <- data.frame(pair   = paste(vars.met[i], vars.met[j], sep=":"), 
+						 v1     = vars.met[i], v2 = vars.met[j],
 						 scale  = scales[s],
-						 cor.est= cor(data.temp[,vars.met[1:2]], use="pairwise.complete.obs")[1,2],
-						 cor.lo = cor.test(data.temp[,vars.met[1]], data.temp[,vars.met[2]], use="pairwise.complete.obs")$conf.int[1],
-						 cor.hi = cor.test(data.temp[,vars.met[1]], data.temp[,vars.met[2]], use="pairwise.complete.obs")$conf.int[2]
+						 cor.est= cor(data.temp[,vars.met[c(i,j)]], use="pairwise.complete.obs")[1,2],
+						 cor.lo = cor.test(data.temp[,vars.met[i]], data.temp[,vars.met[j]], use="pairwise.complete.obs")$conf.int[1],
+						 cor.hi = cor.test(data.temp[,vars.met[i]], data.temp[,vars.met[j]], use="pairwise.complete.obs")$conf.int[2]
 						 )
-	for(i in 2:length(vars.met)){
-		# cor.temp2 <- data.frame(pair = paste(vars.met[i-1], vars.met[i], sep=":"), 
-						   # scale  = scales[s],
-						   # cor.est= cor(data.temp[,vars.met[(i-1):i]], use="pairwise.complete.obs")[2,2],
-						   # cor.lo = cor.test(data.temp[,vars.met[(i-1)]], data.temp[,vars.met[i]], use="pairwise.complete.obs")$conf.int[1],
-						   # cor.hi = cor.test(data.temp[,vars.met[(i-1)]], data.temp[,vars.met[i]], use="pairwise.complete.obs")$conf.int[2]
-						   # )
-		cor.temp <- rbind(cor.temp, data.frame(pair = paste(vars.met[i-1], vars.met[i], sep=":"), 
-						   scale  = scales[s],
-						   cor.est= cor(data.temp[,vars.met[(i-1):i]], use="pairwise.complete.obs")[1,2],
-						   cor.lo = cor.test(data.temp[,vars.met[(i-1)]], data.temp[,vars.met[i]], use="pairwise.complete.obs")$conf.int[1],
-						   cor.hi = cor.test(data.temp[,vars.met[(i-1)]], data.temp[,vars.met[i]], use="pairwise.complete.obs")$conf.int[2]
-						   ))
-	}
-    # cor.temp <- cor(drivers[drivers$Scale==scales[s], vars.met], use="pairwise.complete.obs")
-	# v1 <- rownames(cor.temp)
-	# v2 <- stack(data.frame(cor.temp))[,2]
-	# cor.temp2 <- data.frame(scale=scales[s], pair=paste(v1, v2, sep=":"), v1=v1, v2=v2, var.cor=stack(data.frame(cor.temp))[,1])
-	if(s==1){
-		cor.scales <- cor.temp
-	} else {
-		cor.scales <- rbind(cor.scales, cor.temp)
+			# If this is the first run through, core.scales is that dataframe, otherwise, rbind it
+			if(s==1 & i==1 & j==2){ 
+				cor.scales <- cor.temp 
+			} else {
+				cor.scales <- rbind(cor.scales, cor.temp)
+			}
+		}
 	}
 }
+# Making a sign color to make the graphs; note there's some tricks to getting everything to graph okay
+cor.scales$sign <- as.factor(ifelse(cor.scales$cor.est>=0 | (cor.scales$cor.hi>=0 & !cor.scales$scale=="t.250"), "pos", "neg"))
 summary(cor.scales)
 
 # graph the changes in correlation
+# Note: graphing the absolute value so that going down is always a decrease in correlation
 pdf(file.path(fig.dir, "Driver_Correlations_Across_Scales.pdf"))
-ggplot(data=cor.scales[!cor.scales$v1==cor.scales$v2,]) + facet_grid(v1 ~ v2) +
-	geom_line(aes(x=as.numeric(scale), y=var.cor), size=2) +
-	geom_hline(data=cor.scales[!cor.scales$v1==cor.scales$v2 & cor.scales$scale=="t.001",], aes(yintercept=var.cor), color="red", size=0.5, linetype="dashed") +
-	labs(x="Scale", y="Correlation (R2)", title="Change in Driver Correlation Across Scales") +
+ggplot(data=cor.scales[,]) + facet_grid(v2 ~ v1) +
+	# geom_ribbon(aes(x=as.numeric(scale), ymin=abs(cor.lo), ymax=abs(cor.hi)), fill="red2", alpha=0.7) +
+	geom_hline(data=cor.scales[cor.scales$scale=="t.001",], aes(yintercept=abs(cor.est)), linetype="dashed", size=0.25) +
+	geom_ribbon(data=cor.scales[cor.scales$sign=="neg",], aes(x=as.numeric(scale), ymin=abs(cor.lo), ymax=abs(cor.hi), fill=sign), alpha=0.7) +
+	geom_ribbon(data=cor.scales[cor.scales$sign=="pos",], aes(x=as.numeric(scale), ymin=abs(cor.lo), ymax=abs(cor.hi), fill=sign), alpha=0.7) +
+	# geom_line(aes(x=as.numeric(scale), y=cor.est), size=0.25) +
+	scale_fill_manual(values=c("blue2", "red2")) +
+	guides(fill=guide_legend(title="Corr. Sign")) +
+	labs(x="Scale", y="Absolute Value of Correlation (R2)", title="Change in Driver Correlation Across Scales") +
 	theme_bw()
 dev.off()
-
-	cor2 <- cor.test(drivers[drivers$Scale=="t.001", vars.met[1]], drivers[drivers$Scale=="t.001", vars.met[2]], use="pairwise.complete.obs")
-cor2$conf.int[2]
 # ----------------------------------------
