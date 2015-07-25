060f999155c7720ba807329c428c5126d218593d
crollinson <crollinson@gmail.com>
2015-07-25 19:58:01 -0400

    Allow parallel gamm calculation
    1) Created generalized function (0_gamm.calculate.R) to do the gamm
    analysis for 1 model (1 resolution & 1 extent) at a time.
    
    2) Created script that calculates the “base” effect (annual resolution,
    base extent) for each model in parallel (since it’s taking several
    hours to run.

---
+++ R/0_gamm.calculate.R
@@ -0,0 +1,123 @@
+# ----------------------------------------
+# Making a funciton out of the gamm prediciton script so that it can be farmed out using mclapply
+# Christy Rollinson, crollinson@gmail.com
+# Date Created: 10 July 2015
+# ----------------------------------------
+
+# -----------------
+# Matrix of Models and Drivers
+# -----------------
+# Var    ED2  ED2-LU  CLM-BGC  CLM-CN  LPJ-WSL  LPJ-GUESS  JULES-STAT  JULES-TRIFFID  SIBCASA  LINKAGES
+# tair.gs    X     X        X       X        X         X          X             X           X        X
+# precipf.gs X     X        X       X        X         X          X             X           X        X
+# swdown.gs  X     X        X       X        X         X          X             X           X
+# lwdown.gs  X     X                         X                    X             X           X
+# wind.gs    X     X        X       X                             X             X           X
+# psurf.gs   X     X        X       X                             X             X           X
+# qair.gs    X     X        X       X                             X             X           X
+# CO2.gs     X     X        X       X        X         X          X             X           X
+# Ndep                   ?       ?
+
+
+paleon.gamms.models <- function(data, response, k, predictors.all){
+	# data       = data frame with data for 1 model, 1 extent & 1 resolution
+	# model.name = name of model (goes into data tables to make life easier)
+	# Extent     = the temporal extent of the data (e.g. 850-2010, 1990-2010)
+	# Resolution = the temporal resolution fo the data (t.001 = yearly, etc)
+	# k          = number of knots in gamm splines
+	# predictors.all   = the full set of predictors that should be in data
+	# predictor.suffix = any suffix attached to the predictors; 
+	# 						.yr = yearly mean from drivers; 
+	# 						.gs = growing season mean from drivers; 
+	#						no suffix = yearly mean from model output
+
+
+	# ----------------------------------------
+	# Load Libaries
+	# ----------------------------------------
+	require(parallel)
+	require(mgcv)
+	
+	source('R/0_process.gamm.R', chdir = TRUE)
+	source('R/0_GAMM_Plots.R', chdir = TRUE)
+	# ----------------------------------------
+	# extracting some info previously specified
+	model.name <- unique(data$Model)
+	extent     <- unique(data$Extent)
+	resolution <- unique(data$Resolution)
+
+	# ----------------------------------------
+	# Running the gamm; note this now has AR1 temporal autocorrelation
+	# ----------------------------------------
+	# This is different from model.site.gam in that it has an added random site slope.
+	#	This random effect lets us gauge the overall model.name response to our fixed effects 
+	#   regardless of the site.  
+	#   Pros: Generalized and helps characterize the basic model responses
+	#   Cons: Sloooooooooooow! (or at least slow with the PalEON data set)
+	#------------------------------
+
+	# ----------------------------------------
+	# Select which set of predictors based on which model it is
+	# Each of the models is having different stability issues & has different predictors
+	# ----------------------------------------
+	if(substr(model.name,1,2)=="ed"){
+		predictors <- c("tair", "precipf", "swdown", "lwdown", "psurf", "qair", "wind", "CO2")
+		gam1 <- gamm(NPP ~ s(tair, k=k) + s(precipf, k=k) + s(swdown, k=k) + s(lwdown, k=k) + s(qair, k=k) + s(psurf, k=k) + s(wind, k=k) + s(CO2, k=k) + Site -1, random=list(Site=~Site), data=data, correlation=corARMA(form=~Year, p=1))
+	}
+	if(substr(model.name,1,3)=="clm") {
+		predictors <- c("tair", "precipf", "swdown", "psurf", "qair", "wind", "CO2")
+		# if(model.name=="clm.bgc"){
+			gam1 <- gamm(NPP ~ s(tair, k=k) + s(precipf, k=k) + s(swdown, k=k) + s(qair, k=k) + s(psurf, k=k) + s(wind, k=k) + s(CO2, k=k) + Site -1, random=list(Site=~Site), data=data, correlation=corARMA(form=~Year, p=1), control=list(niterEM=0, sing.tol=1e-20, opt="optim"))
+		# } else {
+			# gam1 <- gamm(NPP ~ s(tair, k=k) + s(precipf, k=k) + s(swdown, k=k) + s(qair, k=k) + s(psurf, k=k) + s(wind, k=k) + s(CO2, k=k) + Site -1, random=list(Site=~Site), data=data, correlation=corARMA(form=~Year, p=1))
+
+		# }
+	}
+	if(substr(model.name,1,3)=="lpj") {
+		predictors <- c("tair", "precipf", "swdown", "CO2")
+		gam1 <- gamm(NPP ~ s(tair, k=k) + s(precipf, k=k) + s(swdown, k=k) + s(CO2, k=k) + Site -1, random=list(Site=~Site), data=data, correlation=corARMA(form=~Year, p=1), control=list(niterEM=0, sing.tol=1e-20, method="optim")) 
+	# , control=list(niterEM=0, sing.tol=1e-20, method="optim")
+	}
+	if(substr(model.name,1,3)=="jul") {
+		predictors <- c("tair", "precipf", "swdown", "lwdown", "psurf", "qair", "wind", "CO2")
+		# if(model.name=="jules.triffid"){
+		# gam1 <- gamm(NPP ~ s(tair, k=k) + s(precipf, k=k) + s(swdown, k=k) + s(lwdown, k=k) + s(qair, k=k) + s(psurf, k=k) + s(wind, k=k) + s(CO2, k=k) + Site -1, random=list(Site=~Site), data=data, correlation=corARMA(form=~Year, p=1), control=list(method="optim"))
+		# } else {
+		gam1 <- gamm(NPP ~ s(tair, k=k) + s(precipf, k=k) + s(swdown, k=k) + s(lwdown, k=k) + s(qair, k=k) + s(psurf, k=k) + s(wind, k=k) + s(CO2, k=k) + Site -1, random=list(Site=~Site), data=data, correlation=corARMA(form=~Year, p=1), control=list(niterEM=0, sing.tol=1e-20, method="optim"))			
+		# }
+		# , control=list(niterEM=0, sing.tol=1e-20, method="optim")
+ 	}
+	if(substr(model.name,1,3)=="sib") {
+		predictors <- c("tair", "precipf", "swdown", "lwdown", "psurf", "qair", "wind", "CO2")
+		gam1 <- gamm(NPP ~ s(tair, k=k) + s(precipf, k=k) + s(swdown, k=k) + s(lwdown, k=k) + s(qair, k=k) + s(psurf, k=k) + s(wind, k=k) + s(CO2, k=k) + Site -1, random=list(Site=~Site), data=data, correlation=corARMA(form=~Year, p=1), control=list(niterEM=0, sing.tol=1e-20, opt="optim"))
+	}
+	if(substr(model.name,1,3)=="lin") {
+		predictors <- c("tair", "precipf")
+		if(resolution=="t.100"){
+		gam1 <- gamm(NPP ~ s(tair, k=k) + s(precipf, k=k) + Site -1, random=list(Site=~Site), data=data, correlation=corARMA(form=~Year, p=1), control=list(niterEM=0, sing.tol=1e-20, opt="optim"))
+		} else {
+		gam1 <- gamm(NPP ~ s(tair, k=k) + s(precipf, k=k) + Site -1, random=list(Site=~Site), data=data, correlation=corARMA(form=~Year, p=1))
+		}
+	}    
+	# ----------------------------------------
+	print("-------------------------------------")
+	print("-------------------------------------")
+	print(paste0("------ Processing Model: ", m.order, " ------"))
+	print(summary(gam1$gam))	
+
+	# get rid of values for predictors not used in the models for clarity later on
+	data[,predictors.all[!(predictors.all %in% predictors)]] <- NA
+
+	# Storing the predicted values from the gam
+	data$fit.gam <- predict(gam1$gam, newdata=data)
+	# ----------------------------------------
+
+	# ----------------------------------------
+	# Run all of the post-processing (calculate CIs, etc)
+	# ----------------------------------------
+	mod.out <- process.gamm(gamm.model=gam1, data=data, model.name=model.name, extent=ext, resolution="t.001", response=response, vars=predictors, write.out=F, outdir=out.dir, fweights=T, ci.model=T, ci.terms=T)
+	# ----------------------------------------
+	
+	return(mod.out)
+}
+
