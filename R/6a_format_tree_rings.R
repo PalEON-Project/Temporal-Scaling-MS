fde02b11dbe1354ec2afaa420caaf0ebc29fc188
crollinson <crollinson@gmail.com>
2015-07-31 17:55:49 -0400

    Changes to Tree Ring processing
    1) Add in some ITRDB files
    2) weight plot-level tree ring data by stem relative importance

--- R/6a_format_tree_rings.R
+++ R/6a_format_tree_rings.R
@@ -63,8 +63,8 @@ if(!dir.exists(dat.base)) dir.create(dat.base)
 if(!dir.exists(fig.base)) dir.create(fig.base)
 
 # Setting the data & figure directories
-fig.dir <- dat.base
-dat.dir <- fig.base
+fig.dir <- fig.base
+dat.dir <- dat.base
 
 # Make sure the appropriate file paths are in place
 if(!dir.exists(dat.dir)) dir.create(dat.dir)
@@ -78,12 +78,14 @@ if(!dir.exists(fig.dir)) dir.create(fig.dir)
 #      -- read in & format column names
 #      -- recode variables for consistent naming conventions
 #      -- add new factors for GAMMs
-#  B) Tree-Ring Data: Harvard Tower, Lyford, Howland
+#  B) Tree-Ring Data: 
+#     Plot-Level: Harvard Tower, Lyford, Howland
+#     ITRDB     : PDL (x5), PHO (x1), PUN (x2)
 # ----------------------------------------
 dir.lyford  <- file.path(in.base, "Lyford_Data_13m_May2015")
 dir.harvard <- file.path(in.base, "HarvardForest")
 dir.howland <- file.path(in.base, "HowlandTreeRings")
-
+dir.itrdb   <- file.path(in.base, "ITRDB")
 # ----------------------------
 # A) Tree Data
 #  Note: lack of consistency among sites mean we have to do this piece-meal rather than smoothly all together;
@@ -105,8 +107,6 @@ trees.lyford$Canopy      <- recode(trees.lyford$Canopy, "'codominant'='C'; 'domi
 trees.lyford$Status       <- recode(trees.lyford$Status, "'alive'='Li'; 'dead'='Sn'")
 summary(trees.lyford)
 
-# Calculate individual density so we can do weighted IV
-
 # ------------
 
 # ------------
@@ -126,8 +126,6 @@ trees.harvard$Canopy      <- recode(trees.harvard$Canopy, "'codominant'='C'; 'do
 trees.harvard$Status       <- recode(trees.harvard$Status, "'dead'='Sn'")
 trees.harvard$Decay        <- as.factor(trees.harvard$Decay)
 summary(trees.harvard)
-
-# Calculate individual density so we can do weighted IV
 
 # ------------
 
@@ -150,8 +148,6 @@ trees.howland$Species     <- recode(trees.howland$Species, "'PCRU'='PIRU'; 'PCRu
 trees.howland$Decay.Class <- as.factor(trees.howland$Decay.Class)
 summary(trees.howland)
 
-# Calculate individual density so we can do weighted IV
-
 # ------------
 
 # ------------
@@ -160,15 +156,48 @@ summary(trees.howland)
 tree.data <- merge(trees.lyford, trees.harvard, all.x=T, all.y=T)
 tree.data <- merge(tree.data, trees.howland, all.x=T, all.y=T)
 summary(tree.data)
+
+# Figuring out the basal area of each tree (for relative importance calculation later on)
+tree.data$BA.tree <- pi*(tree.data$DBH/2)^2
+summary(tree.data)
 # ------------
 # ----------------------------
 
 # ----------------------------
 # Ring Width Data
 # ----------------------------
+# ------------------
+# Plot-Level Tree Rings
+# ------------------
+# PHA
+test <- read.rwl("THIS/IS/A/FOLDER/MINE.rwl")
 rings.harvard <- read.rwl(file.path(dir.harvard, "RW", "TP_All.rwl"))
 rings.lyford  <- read.rwl(file.path(dir.lyford, "RW", "Lyford_All.rwl"))
+
+# PHO
 rings.howland <- read.rwl(file.path(dir.howland, "RawRW_Data_by_sp", "Howland_All.rwl"))
+# ------------------
+
+# ------------------
+# ITRDB Tree Rings
+# ------------------
+# PDL
+dir(file.path(dir.itrdb, "PDL"), ".rwl")
+mn008 <- read.rwl(file.path(dir.itrdb, "PDL", "mn008.rwl.txt"))
+mn009 <- read.rwl(file.path(dir.itrdb, "PDL", "mn009.rwl.txt"))
+mn010 <- read.rwl(file.path(dir.itrdb, "PDL", "mn010.rwl.txt"))
+mn017 <- read.rwl(file.path(dir.itrdb, "PDL", "mn017.rwl.txt"))
+mn019 <- read.rwl(file.path(dir.itrdb, "PDL", "mn019.rwl.txt"))
+
+# PHO
+dir(file.path(dir.itrdb, "PHO"), ".rwl")
+me029 <- read.rwl(file.path(dir.itrdb, "PHO", "me029.rwl.txt"))
+
+# PUN
+dir(file.path(dir.itrdb, "PUN"), ".rwl")
+mi006 <- read.rwl(file.path(dir.itrdb, "PUN", "mi006.rwl.txt"))
+wi002 <- read.rwl(file.path(dir.itrdb, "PUN", "wi002.rwl.txt"))
+# ------------------
 # ----------------------------
 # ----------------------------------------
 
@@ -178,56 +207,159 @@ rings.howland <- read.rwl(file.path(dir.howland, "RawRW_Data_by_sp", "Howland_Al
 # -------------------------------------------------
 # ----------------------------
 # Tree-level detrending
-#  -- NOTE: first pass we're just doing a spline; in the future may want to try something else
+#  -- NOTE: Right now we're just doing a 2/3 spline; in the future may want to try something else; but
+#           Neil thought this sounded fine for now
 # ----------------------------
+# Plot-Level Data
 harvard.detrend <- detrend(rings.harvard, method="Spline")
 lyford.detrend  <- detrend(rings.lyford, method="Spline")
 howland.detrend <- detrend(rings.howland, method="Spline")
+
+# ITRDB data
+## PDL
+mn008.detrend   <- detrend(mn008, method="Spline")
+mn009.detrend   <- detrend(mn009, method="Spline")
+mn010.detrend   <- detrend(mn010, method="Spline")
+mn017.detrend   <- detrend(mn017, method="Spline")
+mn019.detrend   <- detrend(mn019, method="Spline")
+
+## PHO
+me029.detrend   <- detrend(me029, method="Spline")
+
+# PUN
+mi006.detrend   <- detrend(mi006, method="Spline")
+wi002.detrend   <- detrend(wi002, method="Spline")
 # ----------------------------
 
 # ----------------------------
-# Aggregate to the plot level (plot-level chronology)
-#   -- NOTE: first pass is just biweights mean to make a chronology;
-#            in the future, want to weight by tree importance value in the plot to get a weighted mean
+# Merging plot-level data together to do relative importance weighting
+# ----------------------------
+harvard.stack <- stack(harvard.detrend)[c(2,1)]
+names(harvard.stack) <- c("CoreID", "RWI")
+harvard.stack$Year   <- as.numeric(row.names(harvard.detrend))
+harvard.stack$TreeID <- as.factor(substr(harvard.stack$CoreID, 1, 6))
+harvard.stack$PlotID <- as.factor(substr(harvard.stack$CoreID, 1, 3))
+harvard.stack$Site   <- as.factor("PHA")
+summary(harvard.stack)
+
+lyford.stack <- stack(lyford.detrend)[c(2,1)]
+names(lyford.stack) <- c("CoreID", "RWI")
+lyford.stack$Year   <- as.numeric(row.names(lyford.detrend))
+lyford.stack$TreeID <- as.factor(substr(lyford.stack$CoreID, 1, 5))
+lyford.stack$PlotID <- as.factor(substr(lyford.stack$CoreID, 1, 3))
+lyford.stack$Site   <- as.factor("PHA")
+summary(lyford.stack)
+
+howland.stack <- stack(howland.detrend)[c(2,1)]
+names(howland.stack) <- c("CoreID", "RWI")
+howland.stack$Year   <- as.numeric(row.names(howland.detrend))
+howland.stack$TreeID <- as.factor(substr(howland.stack$CoreID, 1, 7))
+howland.stack$PlotID <- as.factor(substr(howland.stack$CoreID, 1, 4))
+howland.stack$Site   <- as.factor("PHO")
+summary(howland.stack)
+
+# Putting all the plot-level ring widths together
+rwi.stack <- rbind(harvard.stack, lyford.stack, howland.stack)
+summary(rwi.stack)
+
+# Aggregate to the tree level
+rwi.stack2 <- aggregate(rwi.stack$RWI, by=list(rwi.stack$Site, rwi.stack$PlotID, rwi.stack$TreeID, rwi.stack$Year), FUN=mean, na.rm=T)
+names(rwi.stack2) <- c("Site", "PlotID", "TreeID", "Year", "RWI")
+rwi.stack2 <- rwi.stack2[complete.cases(rwi.stack2),] # Get rid of missing values because we don't really care
+summary(rwi.stack2)
+
+
+# Merge in the tree-level data
+summary(tree.data[,])
+
+rwi.data <- merge(rwi.stack2, tree.data[,c("TreeID", "PlotID", "Site", "Site2", "Species", "Canopy", "Status", "DBH", "BA.tree")], all.x=T, all.y=F)
+rwi.data <- rwi.data[complete.cases(rwi.data$DBH),] # Get rid of antying without a DBH/Basal Area 
+summary(rwi.data)
+
+# calculate the relative importance of each stem through time
+for(p in unique(rwi.data$PlotID)){
+	years <- unique(rwi.data[rwi.data$PlotID==p, "Year"])
+	for(y in unique(years)){
+		ba.plot <- sum(rwi.data[rwi.data$PlotID==p & rwi.data$Year==y, "BA.tree"])
+		rwi.data[rwi.data$PlotID==p & rwi.data$Year==y, "RI"] <- rwi.data[rwi.data$PlotID==p & rwi.data$Year==y, "BA.tree"]/ba.plot
+	}
+}
+
+summary(rwi.data)
 # ----------------------------
 
-# ------------
-# Creating indexes of which trees are in each plot; doing this the brute-force lazy way for now
-# ------------
-# Harvard Forest Tower
-unique(substr(names(harvard.detrend),1,3))
-harvard1 <- which(substr(names(harvard.detrend),1,3)=="TP1")
-harvard2 <- which(substr(names(harvard.detrend),1,3)=="TP2")
 
-# Lyford Plot (Harvard Forest)
-unique(substr(names(lyford.detrend),1,3))
-lyford1 <- which(substr(names(lyford.detrend),1,3)=="LF1")
-lyford2 <- which(substr(names(lyford.detrend),1,3)=="LF2")
-lyford3 <- which(substr(names(lyford.detrend),1,3)=="LF3")
 
-# Howland Forest
-unique(substr(names(howland.detrend),1,4))
-howland1 <- which(substr(names(howland.detrend),1,4)=="HOW1")
-howland2 <- which(substr(names(howland.detrend),1,4)=="HOW2")
-howland3 <- which(substr(names(howland.detrend),1,4)=="HOW3")
+# ----------------------------
+# Create plot-level "chronologies"
+# ----------------------------
+# ------------
+# Plot-Based Data: do a relative importance-weighted mean of RWI
+# ------------
+rwi.data$RWI.weight <- rwi.data$RWI * rwi.data$RI
+summary(rwi.data)
+
+rwi.plot <- aggregate(rwi.data$RWI.weight, by=list(rwi.data$Site, rwi.data$Site2, rwi.data$PlotID, rwi.data$Year), FUN=sum)
+names(rwi.plot) <- c("Site", "Site2", "PlotID", "Year", "RWI")
+summary(rwi.plot)
 # ------------
 
-# ------------
-# Create plot-level chronologies
-# ------------
-# Harvard Forest Tower
-crn.harvard1 <- chron(harvard.detrend[,harvard1], biweght=T)
-crn.harvard2 <- chron(harvard.detrend[,harvard2], biweght=T)
 
-# Lyford Plot (Harvard Forest)
-crn.lyford1  <- chron(lyford.detrend[,lyford1], biweight=T)
-crn.lyford2  <- chron(lyford.detrend[,lyford2], biweight=T)
-crn.lyford3  <- chron(lyford.detrend[,lyford3], biweight=T)
+# ------------
+# ITRDB Data: Just make a standard biweighted chronology because we don't know anything about the trees themselves
+# ------------
+# PDL
+chron.mn008 <- chron(mn008.detrend, biweight=T)
+chron.mn008$Year <- as.numeric(row.names(chron.mn008))
+chron.mn008$PlotID <- as.factor("mn008")
 
-# Howland Forest Tower
-crn.howland1 <- chron(howland.detrend[,howland1], biweight=T)
-crn.howland2 <- chron(howland.detrend[,howland2], biweight=T)
-crn.howland3 <- chron(howland.detrend[,howland3], biweight=T)
+chron.mn009 <- chron(mn009.detrend, biweight=T)
+chron.mn009$Year <- as.numeric(row.names(chron.mn009))
+chron.mn009$PlotID <- as.factor("mn009")
+
+chron.mn010 <- chron(mn010.detrend, biweight=T)
+chron.mn010$Year <- as.numeric(row.names(chron.mn010))
+chron.mn010$PlotID <- as.factor("mn010")
+
+chron.mn017 <- chron(mn017.detrend, biweight=T)
+chron.mn017$Year <- as.numeric(row.names(chron.mn017))
+chron.mn017$PlotID <- as.factor("mn017")
+
+chron.mn019 <- chron(mn019.detrend, biweight=T)
+chron.mn019$Year <- as.numeric(row.names(chron.mn019))
+chron.mn019$PlotID <- as.factor("mn019")
+
+chron.pdl <- rbind(chron.mn008, chron.mn009, chron.mn010, chron.mn017, chron.mn019)
+names(chron.pdl) <- c("RWI", "n.samples", "Year", "PlotID")
+chron.pdl$Site <- as.factor("PDL")
+summary(chron.pdl)
+
+# PHO
+chron.me029 <- chron(me029.detrend, biweight=T)
+chron.me029$Year <- as.numeric(row.names(chron.me029))
+chron.me029$PlotID <- as.factor("mn019")
+
+chron.pho <- chron.me029
+names(chron.pho) <- c("RWI", "n.samples", "Year", "PlotID")
+chron.pho$Site <- as.factor("PHO")
+summary(chron.pho)
+
+# PUN
+chron.mi006 <- chron(mi006.detrend, biweight=T)
+chron.mi006$Year <- as.numeric(row.names(chron.mi006))
+chron.mi006$PlotID <- as.factor("mi006")
+
+chron.wi002 <- chron(wi002.detrend, biweight=T)
+chron.wi002$Year <- as.numeric(row.names(chron.wi002))
+chron.wi002$PlotID <- as.factor("wi002")
+
+chron.pun <- rbind(chron.mi006, chron.wi002)
+names(chron.pun) <- c("RWI", "n.samples", "Year", "PlotID")
+chron.pun$Site <- as.factor("PUN")
+summary(chron.pun)
+
+# Merge all site-level chronologies together
+chron.itrdb <- rbind(chron.pdl, chron.pho, chron.pun)
 # ------------
 # ----------------------------
 
@@ -235,34 +367,8 @@ crn.howland3 <- chron(howland.detrend[,howland3], biweight=T)
 # Format for GAMM models
 # ----------------------------
 # ------------
-# Bind chronologies together
-# ------------
-# add in plot IDs
-crn.harvard1$PlotID <- "TP1"
-crn.harvard2$PlotID <- "TP2"
-crn.lyford1 $PlotID <- "LF1"
-crn.lyford2 $PlotID <- "LF2"
-crn.lyford3 $PlotID <- "LF3"
-crn.howland1$PlotID <- "HO1"
-crn.howland2$PlotID <- "HO2"
-crn.howland3$PlotID <- "HO3"
-
-
-# add in Years
-crn.harvard1$Year <- as.numeric(row.names(crn.harvard1))
-crn.harvard2$Year <- as.numeric(row.names(crn.harvard2))
-crn.lyford1 $Year <- as.numeric(row.names(crn.lyford1))
-crn.lyford2 $Year <- as.numeric(row.names(crn.lyford2))
-crn.lyford3 $Year <- as.numeric(row.names(crn.lyford3))
-crn.howland1$Year <- as.numeric(row.names(crn.howland1))
-crn.howland2$Year <- as.numeric(row.names(crn.howland2))
-crn.howland3$Year <- as.numeric(row.names(crn.howland3))
-
-# bind the chronologies together
-crn.all <- rbind(crn.harvard1, crn.harvard2, crn.lyford1, crn.lyford2, crn.lyford3, crn.howland1, crn.howland2, crn.howland3)
-crn.all$PlotID <- as.factor(crn.all$PlotID)
-crn.all$Site   <- as.factor(ifelse(substr(crn.all$PlotID,1,2)=="HO", "PHO", "PHA"))
-crn.all$Site2  <- as.factor(substr(crn.all$PlotID, 1, 2))
+# merge chronologies together
+crn.all <- rbind(chron.itrdb[,c("Site", "PlotID", "Year", "RWI")], rwi.plot[,c("Site", "PlotID", "Year", "RWI")])
 summary(crn.all)
 # ------------
 
@@ -274,14 +380,23 @@ sec2yr <- 1*60*60*24*365.25 # 1 sec * 60 sec/min * 60 min/hr * 24 hr/day * 365.2
 met.yr <- read.csv(file.path("Data", "analysis_drivers", "Drivers_Year_GrowingSeason.csv"))
 met.yr$CO2 <- met.yr$CO2.yr 
 met.yr[,substr(names(met.yr),1,6)=="precip"] <- met.yr[,substr(names(met.yr),1,6)=="precip"]*sec2yr
-
 summary(met.yr)
 
-dat.tr <- merge(crn.all, met.yr, all.x=T, all.y=F)
-dat.tr <- dat.tr[complete.cases(dat.tr),]
+# Note: only the met from 1901 & greater is observed (CRUNCEP), so lets not merge in false data
+dat.tr <- merge(crn.all, met.yr[met.yr$Year>=1901,], all.x=T, all.y=F)
+# dat.tr <- dat.tr[complete.cases(dat.tr),]
 summary(dat.tr)
 
+# Save the data
 write.csv(dat.tr, file.path(dat.base, "TreeRing_Chronologies.csv"), row.names=F)
+
+# Lets just look at the chronologies by site
+pdf(file.path(fig.dir, "TreeRingData.pdf"))
+ggplot(dat.tr) + facet_wrap(~Site) +
+	geom_line(aes(x=Year, y=RWI, color=PlotID)) +
+	scale_x_continuous(limits=c(1900,2000)) +
+	theme_bw()
+dev.off()
 # ------------
 # ----------------------------
 # -------------------------------------------------
