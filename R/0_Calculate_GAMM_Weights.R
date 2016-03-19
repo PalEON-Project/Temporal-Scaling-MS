2d2444f60adf383c7e26574ed97cf5f2fcaec9e4
crollinson <crollinson@gmail.com>
2016-03-18 22:36:10 -0400

    do the weights only on splines/continuous variables

--- R/0_Calculate_GAMM_Weights.R
+++ R/0_Calculate_GAMM_Weights.R
@@ -19,8 +19,8 @@ factor.weights <- function(model.gam, model.name, newdata, extent, vars, limitin
 	if(length(intercepts)>0){
 		cols.list[["Site"]] <- intercepts
 	}
-	for(v in vars[!vars=="Y.lag"]){
-		cols.list[[v]] <- which(substr(names(coef.gam),1,(nchar(v)+3))==paste0("s(",v,")"))
+  for(v in vars[!vars=="Y.lag"]){
+    cols.list[[v]] <- which(substr(names(coef.gam),1,(nchar(v)+3))==paste0("s(",v,")"))
 	}
 	# calculating the smoother for each effect; 
 	# now storing everything in a data frame
@@ -33,8 +33,10 @@ factor.weights <- function(model.gam, model.name, newdata, extent, vars, limitin
 		gam.fits[["intercept"]] <- as.vector(t(Xp[,cols.list[["Site"]]]    *  coef.gam[cols.list[["Site"]]])) # Note: no matrix multiplication because it's 1 x 1
 	}
 
+  vars.num <- vector()    
 	for(v in vars){
-		gam.fits[,v] <- as.vector(Xp[,cols.list[[v]]]   %*% coef.gam[cols.list[[v]]])
+	  if(class(newdata[,v]) %in% c("numeric", "integer")) vars.num <- c(vars.num, v)
+    gam.fits[,v] <- as.vector(Xp[,cols.list[[v]]]   %*% coef.gam[cols.list[[v]]])
 	}
 
 	# Calculated the SD around each smoother
@@ -44,9 +46,8 @@ factor.weights <- function(model.gam, model.name, newdata, extent, vars, limitin
 	} else if(length(cols.list[["Site"]])==1){
 		gam.sd[,"intercept"] <-    sum (Xp[,cols.list[["Site"]]]  *  model.gam$Vp[cols.list[["Site"]], cols.list[["Site"]]] * Xp[,cols.list[["Site"]]] )^0.5
 	}
-
-	for(v in vars){
-		gam.sd[,v] <- as.vector(rowSums(Xp[,cols.list[[v]]] %*% model.gam$Vp[cols.list[[v]], cols.list[[v]]] * Xp[,cols.list[[v]]] )^0.5)
+  for(v in vars){
+    gam.sd[,v] <- as.vector(rowSums(Xp[,cols.list[[v]]] %*% model.gam$Vp[cols.list[[v]], cols.list[[v]]] * Xp[,cols.list[[v]]] )^0.5)
 	}
 
 	# Summing the fixed effects to do QA/QC and throwing a warning if it's not very close to the predicted values
@@ -75,15 +76,15 @@ factor.weights <- function(model.gam, model.name, newdata, extent, vars, limitin
   df.weights[,paste("weight", vars, sep=".")] <- NA
   for(i in 1:nrow(df.weights)){
     if(limiting==T){
-      var.min <- min(df.weights[i,paste0("fit.", vars)], na.rm=T)
-      var.max <- max(df.weights[i,paste0("fit.", vars)], na.rm=T)
-      vars.rel <- (var.max-gam.fits[i,vars])/(var.max-var.min) # Basically using distance from max as the relativizer
+      var.min <- min(df.weights[i,paste0("fit.", vars.num)], na.rm=T)
+      var.max <- max(df.weights[i,paste0("fit.", vars.num)], na.rm=T)
+      vars.rel <- (var.max-gam.fits[i,vars.num])/(var.max-var.min) # Basically using distance from max as the relativizer
       weights.vars <- vars.rel/sum(vars.rel) # Finding the proportion of the distance from max of each factor from the total limtiation (dev. from max)
-      df.weights[i,paste("weight", vars, sep=".")] <- weights.vars      
+      df.weights[i,paste("weight", vars.num, sep=".")] <- weights.vars      
     } else {
       # summing the absolute values to get the weights for each fixed effect
-      fit.spline2 <- rowSums(abs(gam.fits[,vars]), na.rm=T)      
-      df.weights[i,paste("weight", vars, sep=".")] <- gam.fits[i,vars]/fit.spline2[i]
+      fit.spline2 <- rowSums(abs(gam.fits[,vars.num]), na.rm=T)      
+      df.weights[i,paste("weight", vars.num, sep=".")] <- gam.fits[i,vars.num]/fit.spline2[i]
     }
 	}
 	
